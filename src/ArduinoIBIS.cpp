0474fba8b15cde2593a07711ba51d26572395175
Jonathan Verbeek <iredcraft@gmail.com>
2025-09-24 17:09:14 +0200

    Initial commit

---
+++ src/ArduinoIBIS.cpp
@@ -0,0 +1,229 @@
+﻿// ArduinoIBIS
+
+#include "ArduinoIBIS.h"
+
+bool ArduinoIBIS::Port::Begin(int8_t txPin, int8_t rxPin, bool invert)
+{
+	// Don't do anything if there's already a port created
+	if (_port != nullptr)
+	{
+		if (_debug) Serial.println("ArduinoIBIS: Cannot begin port as there's a SoftwareSerial open");
+		return false;
+	}
+
+	// Create a new port and begin transmission
+	_port = new EspSoftwareSerial::UART(rxPin, txPin, invert);
+	_port->begin(IBIS_BAUD, IBIS_SERIAL_CONFIG);
+	if ((*_port) == false)
+	{
+		// The port has failed to initialize
+		if (_debug) Serial.print("ArduinoIBIS: Failed to begin SoftwareSerial port");
+		return false;
+	}
+
+	return true;
+}
+
+void ArduinoIBIS::Port::End()
+{
+	if (_port != nullptr)
+	{
+		_port->end();
+		_port = nullptr;
+	}
+}
+
+void ArduinoIBIS::Port::SetEnableDebugOutput(bool enable)
+{
+	_debug = enable;
+}
+
+void ArduinoIBIS::Port::DS010e(const char* sign, uint16_t delay)
+{
+	char buf[IBIS_TELEGRAM_BUFFER_SIZE];
+	int len = sprintf(buf, "xV%.1s%03d", sign, delay);
+
+	String telegram;
+	telegram.concat(buf, len);
+
+	SendTelegram(telegram);
+}
+
+void ArduinoIBIS::Port::DS003a(const String& text)
+{
+	String telegram = "zA";
+
+	uint8_t numBlocks = ceil(text.length() / 16.0);
+	telegram.concat(ToHexString(numBlocks));
+	telegram.concat(text);
+
+	// Fill the remaining string with blank spaces (if any)
+	uint8_t remainder = text.length() % 16;
+	if (remainder > 0)
+	{
+		for (uint8_t i = 16; i > remainder; i--)
+		{
+			telegram.concat(" ");
+		}
+	}
+
+	SendTelegram(telegram);
+}
+
+void ArduinoIBIS::Port::DS003c(const String& text)
+{
+	String telegram = "zI";
+
+	uint8_t numBlocks = ceil(text.length() / 4.0);
+	telegram.concat(ToHexString(numBlocks));
+	telegram.concat(text);
+
+	// Fill the remaining string with blank spaces (if any)
+	uint8_t remainder = text.length() % 4;
+	if (remainder > 0)
+	{
+		for (uint8_t i = 4; i > remainder; i--)
+		{
+			telegram.concat(" ");
+		}
+	}
+
+	SendTelegram(telegram);
+}
+
+void ArduinoIBIS::Port::DS021(uint8_t address, String text)
+{
+	String telegram = "aA";
+
+	uint8_t numBlocks = ceil(text.length() / 4.0);
+	telegram.concat(ToHexString(address));
+	telegram.concat(ToHexString(numBlocks));
+	telegram.concat(text.substring(0, numBlocks * 16));
+
+	SendTelegram(telegram);
+}
+
+void ArduinoIBIS::Port::DS021a(uint8_t address, uint8_t stopId, String stopText, String changeText)
+{
+	String telegram = "aL";
+
+	char data[80];
+	int len = sprintf(data, "\x03%02d\x04%s\x05%s", stopId, stopText.c_str(), changeText.c_str());
+
+	uint8_t numBlocks = ceil(len / 4.0);
+	uint8_t remainder = len % 4;
+	telegram.concat(ToHexString(address));
+	telegram.concat(ToHexString(numBlocks));
+	telegram.concat(ToHexString(remainder));
+	telegram.concat(data, len);
+
+	SendTelegram(telegram);
+}
+
+void ArduinoIBIS::Port::GSP(uint8_t address, String line1, String line2)
+{
+	String lines = "";
+	lines.concat(line1);
+	if (line2.length() > 0)
+	{
+		lines.concat('\x0a'); // LF
+	}
+	lines.concat(line2);
+	lines.concat("\x0a\x0a"); // LF LF
+
+	uint8_t numBlocks = ceil(lines.length() / 16.0);
+	uint8_t remainder = lines.length() % 16;
+	if (remainder > 0)
+	{
+		for (uint8_t i = 16; i > remainder; i--)
+		{
+			lines.concat(" ");
+		}
+	}
+
+	String telegram = "aA";
+	telegram.concat(ToHexString(address));
+	telegram.concat(ToHexString(numBlocks));
+	telegram.concat(lines);
+
+	SendTelegram(telegram);
+}
+
+void ArduinoIBIS::Port::SendTelegram(String telegram)
+{
+	if (_port == nullptr)
+	{
+		if (_debug) Serial.println("ArduinoIBIS: Cannot send, port is null!");
+		return;
+	}
+
+	// German umlauts need to be handled: IBIS telegrams (and any text transmitted inside them) are plain in ASCII format,
+	// where umlauts are not part of. To fix this, the VDV 300 document uses a slightly altered ASCII table, which replaces
+	// a couple of never-used characters from the original ASCII tables with the german umlauts (see VDV 300 page 50).
+	// The replacement of any umlauts with the "real" ASCII values is happening here.
+	telegram.replace("ä", "{");
+	telegram.replace("ö", "|");
+	telegram.replace("ü", "}");
+	telegram.replace("ß", "~");
+	telegram.replace("Ä", "[");
+	telegram.replace("Ö", "\\");
+	telegram.replace("Ü", "]");
+
+	// Add a CR character at the end
+	telegram.concat(static_cast<char>(0x0D));
+
+	// Calculate the telegram checksum by XOR-ing every byte, starting at 0x7F, and append it to the telegram
+	char checksum = 0x7F;
+	for (unsigned int i = 0; i < telegram.length(); i++)
+	{
+		checksum ^= telegram[i];
+	}
+	telegram.concat(checksum);
+
+	// Debug print the whole telegram
+	if (_debug)
+	{
+		Serial.print("ArduinoIBIS: Sending telegram with length=");
+		Serial.print(telegram.length());
+		Serial.print(" checksum=0x");
+		if (checksum < 10)
+		{
+			Serial.print("0");
+		}
+		Serial.print(checksum, HEX);
+		Serial.print(": ");
+
+		for (unsigned int i = 0; i < telegram.length(); i++)
+		{
+			if (telegram.charAt(i) < 0x10)
+			{
+				Serial.print("0");
+			}
+			Serial.print(telegram.charAt(i), HEX);
+			Serial.print(" ");
+		}
+		Serial.println();
+	}
+
+	// Finally send the fully wrapped telegram through the serial port
+	_port->print(telegram);
+}
+
+String ArduinoIBIS::Port::ToHexString(uint8_t value)
+{
+	// The VDV 300 document describes how hex numbers should be encoded (page 50)
+	// Basically, the digits 0..9 are used as-is, and A..F is encoded by :;<=>? respectfully
+	static String hexCharacters = "0123456789:;<=>?";
+	String hexString;
+
+	uint8_t highNibble = value >> 4;
+	uint8_t lowNibble = value & 15;
+
+	if (highNibble > 0)
+	{
+		hexString += hexCharacters.charAt(highNibble);
+	}
+
+	hexString += hexCharacters.charAt(lowNibble);
+	return hexString;
+}
---
+++ src/ArduinoIBIS.h
@@ -0,0 +1,121 @@
+﻿// ArduinoIBIS
+// Implementation of the VDV 300 IBIS Wagenbus protocol for Arduino and Arduino-core based devices
+
+// Copyright (c) 2025 Jonathan Verbeek
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+#include <SoftwareSerial.h>
+
+// IBIS connection parameters (1200 7E2)
+#define IBIS_BAUD 1200
+#define IBIS_SERIAL_CONFIG SWSERIAL_7E2
+
+// Most telegrams don't have a more complex structure and can therefore be constructed with a simple printf string only
+// Hence, IBIS_SIMPLE_TELEGRAM can be used to declare those
+#define IBIS_TELEGRAM_BUFFER_SIZE 64
+#define IBIS_SIMPLE_TELEGRAM(id, argType, fmt) \
+	void DS##id(argType arg) \
+	{ \
+		char buf[IBIS_TELEGRAM_BUFFER_SIZE]; \
+		sprintf(buf, fmt, arg); \
+		SendTelegram(buf); \
+	}
+
+namespace ArduinoIBIS
+{
+	// This class acts as the main communication "port" handling an IBIS device.
+	// IBIS communication happens at 1288 baud in 7E2 configuration and is internally
+	// handled with a software serial interface, which is fine for slow baud rates like this.
+	// This way we're keeping the hardware UART free to use for other uses.
+	class Port
+	{
+	public:
+		// Opens the software serial port for IBIS communication
+		bool Begin(int8_t txPin = 12, int8_t rxPin = -1, bool invert = false);
+
+		// Closes the software serial port
+		void End();
+
+		// When enabled, the library prints debug output to the hardware Serial interface (which still needs to be
+		// initialized by you)
+		void SetEnableDebugOutput(bool enable);
+
+	public:
+		IBIS_SIMPLE_TELEGRAM(001, uint16_t, "l%03d"); // Line Number, 1-3 digits
+		IBIS_SIMPLE_TELEGRAM(001neu, uint16_t, "q%04d"); // Line number, alphanumeric, 1-4 chars
+		IBIS_SIMPLE_TELEGRAM(001a, uint8_t, "lE%02d"); // Line number symbol, 1-2 digits
+		IBIS_SIMPLE_TELEGRAM(001b, uint16_t, "lF%05d"); // Radio
+		IBIS_SIMPLE_TELEGRAM(001c, uint16_t, "lP%03d"); // Line tape reel position ID, 1-3 digits
+		IBIS_SIMPLE_TELEGRAM(001d, uint16_t, "lC%04d"); // Line number, alphanumeric, 1-4 chars
+		IBIS_SIMPLE_TELEGRAM(001e, uint16_t, "lC%08d"); // Line number, alphanumeric, 1-8 chars
+		IBIS_SIMPLE_TELEGRAM(001f, uint16_t, "lC%07d"); // Line number, alphanumeric, 1-7 chars
+		IBIS_SIMPLE_TELEGRAM(002, uint8_t, "k%02d"); // Course number, 1-2 digits
+		IBIS_SIMPLE_TELEGRAM(002a, uint16_t, "k%05d"); // Train number, 1-5 digits
+		IBIS_SIMPLE_TELEGRAM(003, uint16_t, "z%03d"); // Destination text ID, 1-3 digits
+		IBIS_SIMPLE_TELEGRAM(003b, uint16_t, "zR%03d"); // Destination ID for IMU, 1-3 digits
+		IBIS_SIMPLE_TELEGRAM(003d, uint16_t, "zN%03d"); // Route number, 1-3 digits
+		IBIS_SIMPLE_TELEGRAM(003e, uint16_t, "zP%03d"); // Destination tape reel position ID, 1-3 digits
+		IBIS_SIMPLE_TELEGRAM(003f, uint16_t, "zN%06d"); // Route number, 1-6 digits
+		IBIS_SIMPLE_TELEGRAM(003g, uint16_t, "zL%04d"); // Line number, 1-4 digits
+
+		IBIS_SIMPLE_TELEGRAM(004, uint16_t, "e%06d"); // Ticket validator attributes, 6 digits
+		IBIS_SIMPLE_TELEGRAM(004a, uint16_t, "eA%04d"); // Additional ticket validator attributes, 4 digits
+		IBIS_SIMPLE_TELEGRAM(004b, uint16_t, "eH%07d"); // Ticket validator stop number, 1-7 digits
+
+		IBIS_SIMPLE_TELEGRAM(005, uint16_t, "u%04d"); // Time, HHMM
+		IBIS_SIMPLE_TELEGRAM(006, uint16_t, "d%05d"); // Date, DDMMY
+
+		IBIS_SIMPLE_TELEGRAM(007, uint8_t, "w%01d"); // Train length, 1 digit
+
+		IBIS_SIMPLE_TELEGRAM(009, const char*, "v%-16s"); // Next stop text, 16 chars
+		IBIS_SIMPLE_TELEGRAM(009a, const char*, "v%-20s"); // Next stop text, 20 chars
+		IBIS_SIMPLE_TELEGRAM(009b, const char*, "v%-24s"); // Next stop text, 24 chars
+
+		IBIS_SIMPLE_TELEGRAM(010, uint16_t, "x%04d"); // Line progress display stop ID, 1-4 digits
+		IBIS_SIMPLE_TELEGRAM(010a, uint16_t, "xH%04d"); // Line progress display stop ID, 1-4 digits
+		IBIS_SIMPLE_TELEGRAM(010b, uint8_t, "xI%02d"); // Line progress display stop ID, 1-2 digits
+		IBIS_SIMPLE_TELEGRAM(010d, uint16_t, "xJ%04d"); // Year, YYYY
+
+		void DS010e(const char* sign, uint16_t delay); // Delay, sign is either '+' or '-', delay is 1-3 digits
+
+		void DS003a(const String& text); // Destination text
+		void DS003c(const String& text); // Next stop name
+
+		void DS021(uint8_t address, String text); // Destination text
+		void DS021a(uint8_t address, uint8_t stopId, String stopText, String changeText); // Line progress display text
+
+		void GSP(uint8_t address, String line1, String line2);
+
+	private:
+		// Wraps the telegram with required extra data and sends it to the serial port
+		void SendTelegram(String telegram);
+
+		// Converts a uint8 value to a VDV hex string
+		static String ToHexString(uint8_t value);
+
+	private:
+		// Internal handle to the software serial port
+		EspSoftwareSerial::UART* _port = nullptr;
+
+		// Whether to print debug output to Serial0
+		bool _debug = false;
+	};
+}
